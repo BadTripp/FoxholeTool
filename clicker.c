d6c6f8761cbce4c4a02f2669b987c729330c5822
BadTripp <renato.turetta00@gmail.com>
2026-01-16 17:29:37 +0100

    Refactor msleep function to use nanosleep for better precision and added error handling for non-positive sleep durations.

--- clicker.c
+++ clicker.c
@@ -19,6 +19,7 @@
 #include <stdint.h>
 #include <stdatomic.h>
 #include <time.h>
+#include <errno.h>
 #include <ctype.h>
 #include <string.h>
 #include <unistd.h>     //usleep, readlink
@@ -97,7 +98,11 @@ static void msleep(int ms) {
 #ifdef _WIN32
   Sleep((DWORD)ms);
 #else
-  usleep((useconds_t)ms * 1000u);
+  if (ms <= 0) return;
+  struct timespec ts;
+  ts.tv_sec  = ms / 1000;
+  ts.tv_nsec = (long)(ms % 1000) * 1000000L;
+  nanosleep(&ts, NULL);
 #endif
 }
 
